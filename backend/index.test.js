ba63ee158846b963572cb25db6aba4ba778cb8b5
jburn <jopbruun@gmail.com>
2025-01-30 14:10:37 +0200

    fix e2e

--- backend/index.test.js
+++ backend/index.test.js
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from './index.js'
 
-test('Get helloworld from root url', async () => {
+test('Get helloworld from url', async () => {
     const resp = await request(app).get('/hello')
     expect(resp.statusCode).toBe(200)
     expect(resp.text).toBe('<h1>Hello World!</h1>')
