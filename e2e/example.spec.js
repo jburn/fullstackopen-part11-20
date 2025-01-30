0af632e1e7276ab62da06e93351a708f4d8cbb5b
jburn <jopbruun@gmail.com>
2025-01-30 10:56:07 +0200

    improvement

--- e2e/example.spec.js
+++ e2e/example.spec.js
@@ -3,4 +3,5 @@ import { test, expect } from '@playwright/test'
 test('phonebook opens', async ({ page }) => {
   await page.goto('/')
   await expect(page.getByText('Phonebook')).toBeVisible()
+  await expect(page.getByTestId('deleteButton').first()).toBeVisible()
 })
